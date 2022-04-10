705078783544ddaaaffa881a63008c64c4f134d4
AshSmol <ashiryaev84@gmail.com>
2022-04-10 22:07:06 +0300

    Default settings.py changed for live postgresql settings db. Added settings_dev.py containing db settings for development.

--- kbank/kbank/settings_dev.py
+++ kbank/kbank/settings_dev.py
@@ -74,17 +74,9 @@ WSGI_APPLICATION = 'kbank.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kbank',
-        'USER': 'django',
-        'PASSWORD': 'KBANK',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
