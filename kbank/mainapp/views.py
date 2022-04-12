e70dbe4292e73828fc6f3a21283eb596d19bca17
AshSmol <ashiryaev84@gmail.com>
2022-04-13 00:01:32 +0300

    added main page view and controller. Experimental variant

--- kbank/mainapp/views.py
+++ kbank/mainapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mainapp.models import Article
+
+
+class ArticlesListView(ListView):
+    model = Article
+    template_name = 'mainapp/index.html'
+    context_object_name = 'articles'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ArticlesListView, self).get_context_data()
+        context['title'] = 'Статьи участников'
+
+        return context
+
+    def get_queryset(self):
+        return Article.objects.all()
