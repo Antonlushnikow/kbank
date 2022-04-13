56d3a2246f5170c3c7cd6654547bd26666c05150
Anton Lushnikov <antonlushnikow@gmail.com>
2022-04-13 20:51:19 +0500

    Added CreateArticle Page, Controller and Form

--- kbank/mainapp/models.py
+++ kbank/mainapp/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.conf import settings
+from tinymce.models import HTMLField
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=60)
-    text = models.TextField()
-    publish_date = models.DateField(auto_now_add=True)
-    updated_date = models.DateField(auto_now=True)
+    title = models.CharField(max_length=60, verbose_name='заголовок')
+    text = HTMLField(verbose_name='текст')
+    publish_date = models.DateField(auto_now_add=True, verbose_name='дата публикации')
+    updated_date = models.DateField(auto_now=True, verbose_name='дата изменения')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='автор'
     )
 
     class Meta:
