e3dfca31a25ae62e8518c0fcdfd4d7f8d1697e57
AshSmol <ashiryaev84@gmail.com>
2022-04-16 13:13:20 +0300

    #010 Registration page, controller, urls. Added registration page link to login.html page. Fix footer at the bottom of the page

--- kbank/authapp/views.py
+++ kbank/authapp/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
 
 from .models import KbankUser
-from .forms import KbankUserLoginForm
+from .forms import KbankUserLoginForm, KbankUserRegistrationForm
 
 
 class KbankUserLoginView(LoginView):
@@ -24,3 +26,21 @@ class KbankUserLogoutView(LogoutView):
 
     def get_next_page(self):
         return reverse('index')
+
+
+class KbankUserRegisterView(CreateView):
+    Model = KbankUser
+    form_class = KbankUserRegistrationForm
+    template_name = 'authapp/registration.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(KbankUserRegisterView, self).get_context_data()
+        context['title'] = 'регистрация'
+        return context
+
+    def get_success_url(self):
+        return reverse('authapp:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        return HttpResponseRedirect(reverse('authapp:login'))
