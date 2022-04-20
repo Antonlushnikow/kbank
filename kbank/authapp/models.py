cf21fbec20cee5284afec428e300aed5613c86af
Anton Lushnikov <antonlushnikow@gmail.com>
2022-04-20 17:17:30 +0500

    Add profile menu

--- kbank/authapp/models.py
+++ kbank/authapp/models.py
@@ -6,6 +6,7 @@ class KbankUser(AbstractUser):
     info = models.TextField(max_length=128, blank=True, verbose_name="о себе")
     is_deleted = models.BooleanField(default=False)
     avatar = models.ImageField(
-        upload_to='users_avatar',
+        upload_to='users_avatar/',
         blank=True,
+        default='/users_avatar/default.png'
     )
