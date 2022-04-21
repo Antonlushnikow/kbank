9972700e8db988a04b2a9e41eaaf38a98182a9ad
Anton Lushnikov <antonlushnikow@gmail.com>
2022-04-21 10:51:55 +0500

    Add likes for articles and comments

---
+++ kbank/kbank/static/kbank/update-likes.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.like-btn').click(function(e){
+        e.preventDefault();
+
+        const likeURL = $(this).attr("data-href");  // API URL
+        const objectId = $(this).attr("data-id");
+        let likeCount = $(`#like-count-${objectId}`);  // Total count of likes
+        let likeIcon = $(`.like-icon-${objectId}`);  // Font-awesome icon
+
+        $.ajax({
+            url: likeURL,
+            method: "GET",
+            data: {},
+            success: function(data){
+                // Icon change
+                if (data.liked) {
+                    likeIcon[0].classList.remove("fa-regular");
+                    likeIcon[0].classList.add("fa-solid");
+                } else {
+                    likeIcon[0].classList.remove("fa-solid");
+                    likeIcon[0].classList.add("fa-regular");
+                }
+                // Update like count from API
+                likeCount[0].innerText = data.like_count;
+            },
+            error: function(error){
+                console.log(error)
+            }
+        });
+    });
+});
